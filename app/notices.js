dc7eb2965bf10c97c7b8e4b79b400fcde632c3b1
Adrian Zalewski <adrian.zalewski@netguru.pl>
2016-06-24 15:02:25 +0200

    notices: dedicated function for getting connection-gone notice data

--- app/notices.js
+++ app/notices.js
@@ -1,15 +1,31 @@
 import notifier from 'node-notifier';
 import player from 'play-sound';
+import path from 'path';
 
 const myPlayer = player();
+
+function getRandomNoticeDataGone() {
+  const soundFilePath = path.resolve('sounds/gone/linda-wypierdalac.mp3');
+
+  return {
+    notification: {
+      title: 'Internet is gone - title',
+      subtitle: 'Internet is gone - subtitle',
+      message: 'Internet is gone - message',
+    },
+    sound: soundFilePath,
+  };
+}
 
 function notifyInternetConnectionGotIt() {
   notifier.notify('Got Internet!');
 }
 
 function notifyInternetConnectionGone() {
-  myPlayer.play('sounds/gone/linda-wypierdalac.mp3', (err) => console.log(err));
-  notifier.notify('F**ity f**k, it\'s gone...');
+  const data = getRandomNoticeDataGone();
+
+  myPlayer.play(data.sound, (err) => err && console.log(err));
+  notifier.notify(data.notification);
 }
 
 export default {
