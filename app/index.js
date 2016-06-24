6ec4b87f6f324b89d1c3d34affba0a30fee38234
Adrian Zalewski <adrian.zalewski@netguru.pl>
2016-06-24 13:10:09 +0200

    index file: initial, recurring connection check

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
-'use strict';
+import isOnline from 'is-online';
+
+function check() {
+  isOnline((err, online) => {
+    console.log(online);
+
+    setTimeout(check, 3000);
+  });
+}
+
+check();
 
 console.log('hello world!');
